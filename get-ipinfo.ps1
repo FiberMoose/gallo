9bf887b6221adcbb9841312551f74cecc34e8031
FiberMoose <1664727+FiberMoose@users.noreply.github.com>
2022-09-14 08:17:54 -0500

    Added IPV6 Support! Also reworked json output

--- get-ipinfo.ps1
+++ get-ipinfo.ps1
@@ -39,48 +39,60 @@ $varIP = ${IP Address}
 
 $ErrorActionPreference = "silentlycontinue"
 
-#Define IP Regex in variables
+#Define IP Regex
 $varIPv4PrivateRegex = "(^127\.)|(^192\.168\.)|(^10\.)|(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)"
 $varIPv4ValidRegex = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
-$varIPv6Regex = ":(?::[a-f\d]{1,4}){0,5}(?:(?::[a-f\d]{1,4}){1,2}|:(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})))|[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}|:)|(?::(?:[a-f\d]{1,4})?|(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))))|:(?:(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|[a-f\d]{1,4}(?::[a-f\d]{1,4})?|))|(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|:[a-f\d]{1,4}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){0,2})|:))|(?:(?::[a-f\d]{1,4}){0,2}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){1,2})|:))|(?:(?::[a-f\d]{1,4}){0,3}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){1,2})|:))|(?:(?::[a-f\d]{1,4}){0,4}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){1,2})|:))"
+$varIPv6ValidRegex = ":(?::[a-f\d]{1,4}){0,5}(?:(?::[a-f\d]{1,4}){1,2}|:(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})))|[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}:(?:[a-f\d]{1,4}|:)|(?::(?:[a-f\d]{1,4})?|(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))))|:(?:(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|[a-f\d]{1,4}(?::[a-f\d]{1,4})?|))|(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|:[a-f\d]{1,4}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){0,2})|:))|(?:(?::[a-f\d]{1,4}){0,2}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){1,2})|:))|(?:(?::[a-f\d]{1,4}){0,3}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){1,2})|:))|(?:(?::[a-f\d]{1,4}){0,4}(?::(?:(?:(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2})\.){3}(?:25[0-5]|2[0-4][0-9]|[0-1]?[0-9]{1,2}))|(?::[a-f\d]{1,4}){1,2})|:))"
+$varIPv6PrivateRegex = "(^::1$)|(^[fF][cCdD])" 
 
 
 function ValidateIP {
     #IP Validation function
     param([string]$varIPValidate)
-    [bool](($varIP -match $varIPv4ValidRegex -and [bool]($varIPValidate -as [ipaddress]) -and !($varIPValidate -Match $varIPv4PrivateRegex)))
+    [bool](((($varIP -match $varIPv4ValidRegex) -OR ($varIP -match $varIPv6ValidRegex)) -and (!($varIPValidate -Match $varIPv4PrivateRegex)) -and (!($varIPValidate -Match $varIPv6PrivateRegex))))
 }
 
 function ValidateIPDescriptive {
+    #IP validation function with detailed error message
     param([string]$varIPValidate)
-    if ($varIPValidate -Match $varIPv6Regex) {
-        #Check if iP is an IPv6 address
-        "ERROR: IPv6 Address provided. Unsupported in this revision."
-    } elseif ($varIPValidate -Match $varIPv4PrivateRegex) {
-        #Check if IP is a private non-routable
+    if ($varIPValidate -Match $varIPv4PrivateRegex) {
+        #Check if IP is a private non-routable ipv4
         "ERROR: Non-routable IPv4 Address provided."
-    } elseif (!($varIPValidate -match $varIPv4ValidRegex)) {
+    } elseif ($varIPValidate -Match $varIPv6PrivateRegex) {
+        #Check if IP is a private non-routable ipv6
+        "ERROR: Non-routable IPv6 Address provided."
+    } elseif ((!($varIPValidate -match $varIPv4ValidRegex)) -and (!($varIPValidate -match $varIPv6ValidRegex))) {
         #Check if IP is a valid formatted IPv4 IP Address
-        "ERROR: Invalid IPv4 Address provided."
+        "ERROR: Invalid IP Address provided."
     } else {
-        "ERROR: Invalid Address provided" 
+        "ERROR: Invalid IP Address provided" 
     }
+}
+Function IPV6ValidRoutablecheck {
+    param([string]$varIPValidate)
+    [bool](($varIPValidate -Match $varIPv6ValidRegex) -and (!($varIPValidate -Match $varIPv6PrivateRegex))) 
+    #Check if iP is an public IPv6 address
+}
+
+if (IPV6ValidRoutablecheck ($varIP)) {
+    $varIPType = "IPv6"
+} else {
+    $varIPType = "IPv4"
 }
 
 if (($varIP -ne $null) -and (ValidateIP ($varIP) -eq $true)) {
-    if ($json) {
-        #check if the $json switch is set to true
-        function write-host {
-            #If $json = true, overide write-host to output nothing ($null)
-            $null
+    if (!($json)) {
+        write-host "`nA valid routable $varIPType address was Supplied!" $varIPValidate
+        if ($varIPType -eq "IPv6") {
+            write-host "WARNING: IPV6 Addresses are not always reliably queried. Expect erratic behavior." -ForegroundColor yellow
         }
+        write-host "`nVerifying if $varIP is currently responding to ICMP requests`n"
     }
-
-    write-host "`nA valid routable IPv4 address was Supplied!" $varIPValidate
     
-    write-host "Verifying if $varIP is currently responding to ICMP requests`n"
     if (Test-connection $varIP -count 1) {
-        write-host "Success!`nPlease wait while we run a traceroute and ping test...`n" -ForegroundColor green
+        if (!($json)) {
+            write-host "Success!`nPlease wait while we run a traceroute and ping test...`n" -ForegroundColor green
+        }
         $varHops = (Test-NetConnection -TraceRoute -ComputerName $varIP).traceroute.count #Measure hops between script host and IP
         if ($PSVersionTable.PSVersion.major -gt "5") {
             #Workaround for changes in Test-Connection sytax post Powershell Version 5.1
@@ -88,59 +100,75 @@ if (($varIP -ne $null) -and (ValidateIP ($varIP) -eq $true)) {
         } else {
             $varPingProperty = "ResponseTime"
         }
+        #Test-connection has problems with IPv6 on some systems. 
         $varPingAvg = ((Test-Connection $varIP -count 10).$varPingProperty | Measure-Object -Average).Average #measure ICMP latency 10 times
             
-            
-        write-host "There are a total of $varHops hops between this host ($env:computername) and the supplied IP $varIP."
-        write-host "The average packet latency is $varPingAvg ms.`n"
+        if (!($json)) {    
+            write-host "There are a total of $varHops hops between this host ($env:computername) and the supplied IP $varIP."
+            write-host "The average packet latency is $varPingAvg ms.`n"
+        }
 
     } else {
-        Write-host "Error! We were not able to reach" $varIP "`n"-ForegroundColor red
-        $varPingAvg = "Error: Ping failed"
-        $varHops = "Error: Traceroute failed"
-        $varIPTestsError = "ERROR: Ping and or Traceroute failed! "
+        if (!($json)) {
+            Write-host "Error! We were not able to reach" $varIP "`n"-ForegroundColor red
+            $varPingAvg = "Error: Ping failed"
+            $varHops = "Error: Traceroute failed"
+        }
+        $varIPTestsError = $error[0].ToString()
     }
 
     $header = @{"Accept" = "application/xml" }
     $varWhois = Invoke-RestMethod -Method Get "http://ip-api.com/json/$varIP" -Headers $header
-    if (($varWhois.status -eq "success") -and ($varWhois.query -eq $varIP)) {
-        $varWhoisLocation = $varWhois.City + ", " + $varWhois.Region + ", " + $varWhois.country
-        $varGeoWeather = Invoke-RestMethod -Method Get -Uri ("http://api.weatherapi.com/v1/current.json?key=c90129e9d8c843869b350836221009&q=" + $varWhoisLocation) -Headers $header
-        $varGeoLocalTime24 = ([string]($varGeoWeather.location.localtime).Split(' ')[1].PadLeft(5,'0'))
-        $varSplit = (([datetime]::ParseExact($varGeoLocalTime24, 'HH:mm', $null)).ToString()).Split(" ")
-        $varGeoLocalTime12 = [string]$varSplit[1..($varSplit.count - 1)]
+    $varWhoisLocation = $varWhois.City + ", " + $varWhois.Region + ", " + $varWhois.country
+    $varGeoWeather = Invoke-RestMethod -Method Get -Uri ("http://api.weatherapi.com/v1/current.json?key=c90129e9d8c843869b350836221009&q=" + $varWhoisLocation) -Headers $header
 
-        write-host "The IP $varIP is:"
-        write-host "    Owned by:" $varWhois.org
-        write-host "    Belongs to the ASN:" ($varWhois.as).Split(' ')[0] "owned by"(($varWhois.as) -split ' ', 2)[-1]
-        write-host "    Has a recorded ISP of:" $varWhois.isp
-        write-host "    Geo-located to the general area of:" $varWhoisLocation"`n"
-        write-host "Currently in" $varWhois.City "it is $varGeoLocalTime12 localtime, with weather conditions of:"
-        write-host "   "$varGeoWeather.current.temp_f "degrees fahrenheit and"$varGeoWeather.current.condition.text""
-        write-host "    A humidity of"$varGeoWeather.current.humidity "%"
-        write-host "    A Windspeed of"$varGeoWeather.current.wind_mph"mph"
+    if ($varWhois.status -eq "success") {
+        if (($varGeoWeather.current.temp_f -gt "0") -and ($varGeoWeather.location.name -contains $varWhois.City)) {
+            $varGeoLocalTime24 = ([string]($varGeoWeather.location.localtime).Split(' ')[1].PadLeft(5, '0'))
+            $varSplit = (([datetime]::ParseExact($varGeoLocalTime24, 'HH:mm', $null)).ToString()).Split(" ")
+            $varGeoLocalTime12 = [string]$varSplit[1..($varSplit.count - 1)]
+            if (!($json)) {
+                write-host "The IP $varIP is:"
+                write-host "    Owned by:" $varWhois.org
+                write-host "    Belongs to the ASN:" ($varWhois.as).Split(' ')[0] "owned by"(($varWhois.as) -split ' ', 2)[-1]
+                write-host "    Has a recorded ISP of:" $varWhois.isp
+                write-host "    Geo-located to the general area of:" $varWhoisLocation"`n"
+                write-host "Currently in" $varWhois.City "it is $varGeoLocalTime12 localtime, with weather conditions of:"
+                write-host "   "$varGeoWeather.current.temp_f "degrees fahrenheit and"$varGeoWeather.current.condition.text""
+                write-host "    A humidity of"$varGeoWeather.current.humidity "%"
+                write-host "    A Windspeed of"$varGeoWeather.current.wind_mph"mph"
+            }
+        } else {
+            if (!($json)) {
+                Write-host "There was a problem querying Weather and Time Information" -ForegroundColor red
+                write-host "The returned response was: `n" ($varGeoWeather | convertto-json)
+            }
+            $varWeatherError = "ERROR: Weather query failed."
+        }
     } else {
-        Write-host "There was a problem querying Whois Information" -ForegroundColor red
-        write-host "The returned response was: " $varWhois
+        if (!($json)) {
+            Write-host "There was a problem querying Whois Information" -ForegroundColor red
+            write-host "The returned response was: `n" ($varWhois | convertto-json)
+        }
         $varWhoisError = "ERROR: Whois query failed."
     }
 
-    $varErrors = $varIPTestsError + $varWhoisError
 
     if ($json) {
         #IF Json script parameter is set to true
         $varjsondata = @{
             'IPAddress'     = "$($varIP)"
-            'ERROR'         = "$($varErrors)"
             'Network_Tests' = @{
                 'Hops'        = "$($varHops)"
                 'Latency_avg' = "$($varPingAvg)"
+                'ERROR'       = "$($varIPTestsError)"
             }
             'IP_Whois'      = @{
                 'Netblock_Owner' = "$($varWhois.org)"
                 'ASN'            = "$(($varWhois.as).Split(' ')[0])"
                 'ASN_Owner'      = "$((($varWhois.as) -split ' ',2)[-1])"
                 'ISP'            = "$($varWhois.isp)"
+                'ERROR'          = "$($varWhoisError)"
             }
             'IPGeoLocation' = @{
                 'City'    = "$($varWhois.City)"
@@ -153,6 +181,7 @@ if (($varIP -ne $null) -and (ValidateIP ($varIP) -eq $true)) {
                 'Humidity'      = "$($varGeoWeather.current.humidity)"
                 'Windspeed'     = "$($varGeoWeather.current.wind_mph)"
                 'Condition'     = "$($varGeoWeather.current.condition.text)"
+                'ERROR'         = "$($varWeatherError)"
             }
             'Date_Time'     = @{
                 'Time_24_HHmm'    = "$($varGeoLocalTime24)"
@@ -167,6 +196,7 @@ if (($varIP -ne $null) -and (ValidateIP ($varIP) -eq $true)) {
     #exit 0
     
 } elseif ($json) {
+    #error output if IP provided is invalid and json switch set
     $varjsondata = @{
         'IPAddress' = "$($varIP)"
         'ERROR'     = "$(ValidateIPDescriptive ($varIP))"
